1f51e2a53c1c1510a84b7a1716b36539552346ad
vamshi-theaicloud <vamshi@theai.cloud>
2025-03-24 19:05:29 +0530

    fixed cou_course models and schema 24032025

--- cou_course/models/course.py
+++ cou_course/models/course.py
@@ -1,4 +1,3 @@
-# cou_admin/models/country.py
 from sqlmodel import SQLModel, Field, Relationship
 from typing import Optional, TYPE_CHECKING
 from datetime import datetime, timezone
@@ -12,11 +11,13 @@ class Course(SQLModel, table=True):
 
     id: Optional[int] = Field(default=None, primary_key=True)
     title: str = Field(max_length=255)
+    what_will_you: Optional[str] = None
     description: Optional[str] = None
-    category_id: Optional[int] = Field(default=None, foreign_key="cou_course.category.id")
-    subcategory_id: Optional[int] = Field(default=None, foreign_key="cou_course.subcategory.id")
-    course_type_id: Optional[int] = Field(default=None, foreign_key="cou_course.coursetype.id")
-    sells_type_id: Optional[int] = Field(default=None, foreign_key="cou_course.sellstype.id")
+    category_id: Optional[int] = Field(default=None, foreign_key="cou_course.course_category.id")
+    subcategory_id: Optional[int] = Field(default=None, foreign_key="cou_course.course_subcategory.id")
+    course_type_id: Optional[int] = Field(default=None, foreign_key="cou_course.course_type.id")
+    sells_type_id: Optional[int] = Field(default=None, foreign_key="cou_course.sells_type.id")
+    mentor_id: Optional[int] = Field(default=None, foreign_key="cou_user.user.id")
     language_id: Optional[int] = Field(default=None, foreign_key="cou_admin.language.id")
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
@@ -24,9 +25,17 @@ class Course(SQLModel, table=True):
     updated_by: Optional[int] = None
     is_flagship: Optional[bool] = Field(default=False)
     active: Optional[bool] = Field(default=True)
-    ratings: Optional[float] = Field(default=0.0)
     price: Optional[float] = Field(default=0.0)
-    mentor_id: Optional[int] = Field(default=None, foreign_key="cou_user.user.id")
+    ratings: Optional[int] = None
+    slug: Optional[str] = None
+    discount: Optional[float] = None
+    introduction_video: Optional[str] = None
+    prerequisites: Optional[str] = None
+    skill_level: Optional[str] = None
+    max_students: Optional[float] = None
+    thumbnail: Optional[str] = None
+    time: Optional[float] = None
+    is_live: Optional[bool] = None
     instructor: Optional["User"] = Relationship(
         back_populates="courses",
         sa_relationship_kwargs={"lazy": "joined"}
